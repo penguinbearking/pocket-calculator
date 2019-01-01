f21cb35122dac06b00478ab30149a629e51a9239
penguinbearking <snarendran@ucvts.org>
2019-01-01 15:12:46 -0500

    I have no idea what I did

--- script/mainscript.js
+++ script/mainscript.js
@@ -1,16 +1,19 @@
 //JS main script
 
-function startup() {
-	var reg = true;
-	var graph = false;
-	var mem0;
-	var mem1;
-	var mem2;
-	var mem3;
 
-	var numarr0 = [];
-	var numarr1 = [];
-	var numarr2 = [];
-	var numarr3 = [];
+var reg = true;
+var graph = false;
+var mem0;
+var mem1;
+var mem2;
+var numarr0 = [];
+var numarr1 = [];
+var numarr2 = [];
+var numarr3 = [];
+var numstr0 = '';
+var lastnum = 0;
+
+function startup() {
+	
 
 }
